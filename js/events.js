5cf29df00f8f4b8d7c3ac79d2f13c0625ecb1085
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 04:21:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,12 @@ function pressIt () {
 }
 
 function submitIt () {
-
+  $("form").on("submit", function() {
+    if ($( "input:first" ).val() === "correct") {
+      alert('your form is going to be submitted now');
+      return;
+    }
+  });
 }
 
 $(document).ready(function(){
