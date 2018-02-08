a6290fe49e3f35ac00890db5acfb18c1d0eb8ddc
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 04:24:40 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,10 +21,8 @@ function pressIt () {
 
 function submitIt () {
   $("form").on("submit", function() {
-    if ($( "input:first" ).val() === "correct") {
       alert('Your form is going to be submitted now.');
       return;
-    }
   });
 }
 
