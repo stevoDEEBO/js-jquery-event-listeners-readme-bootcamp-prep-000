2cdc4162c2d95d9bf0be3f3b241b11facf6b486f
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 04:08:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,21 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+});
+}
+
+function frameIt () {
+  
+}
+
+function pressIt () {
+  
+}
+
+function submitIt () {
+  
+}
 
 $(document).ready(function(){
 
