4752321baf21c3a54f67e8e09a201a489f24557c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 04:09:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,19 +2,19 @@
 function getIt() {
   $('p').on('click', function() {
     alert('Hey!');
-});
+  });
 }
 
 function frameIt () {
-  
+
 }
 
 function pressIt () {
-  
+
 }
 
 function submitIt () {
-  
+
 }
 
 $(document).ready(function(){
